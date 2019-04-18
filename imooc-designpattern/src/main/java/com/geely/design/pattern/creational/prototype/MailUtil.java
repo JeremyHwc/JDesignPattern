e6535b6c475593a297a0bb3724a8e35c3f12e550
JeremyHwc <jeremy_hwc@163.com>
2019-04-18 18:28:15 +0800

    学习单例模式

--- imooc-designpattern/src/main/java/com/geely/design/pattern/creational/prototype/MailUtil.java
+++ imooc-designpattern/src/main/java/com/geely/design/pattern/creational/prototype/MailUtil.java
@@ -10,6 +10,7 @@ public class MailUtil {
         String outputContent = "向{0}同学,邮件地址:{1},邮件内容:{2}发送邮件成功";
         System.out.println(MessageFormat.format(outputContent,mail.getName(),mail.getEmailAddress(),mail.getContent()));
     }
+
     public static void saveOriginMailRecord(Mail mail){
         System.out.println("存储originMail记录,originMail:"+mail.getContent());
     }
